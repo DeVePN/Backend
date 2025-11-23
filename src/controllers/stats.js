e9e074c25ea3927f5240e3bdaae44a375ba2d0bf
Muhamad Harfi Ibadurahman <harfi@ASURA-2.local>
2025-11-23 21:16:02 +0700

    fix wrong path

--- src/controllers/stats.js
+++ src/controllers/stats.js
@@ -1,4 +1,4 @@
-import { asyncHandler } from '../utils/asyncHandler.js';
+import { asyncHandler } from '../middleware/errorHandler.js';
 import { getUserSessions } from '../services/supabase.js';
 
 /**
